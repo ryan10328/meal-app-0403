eda00e582b6048ea09ab4bc83f651884dc17bb45
Ryan Tseng <ryan10328@gmail.com>
2025-04-14 00:40:39 +0800

    113. App Finishing Touches

--- screens/drawer/DrawerNavigator.tsx
+++ screens/drawer/DrawerNavigator.tsx
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { DrawerScreenName } from "../../types/navigation";
 import CategoriesScreen from "../CategoriesScreen";
 import FavoriteScreen from "../FavoriteScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
 
@@ -12,6 +13,10 @@ const DrawerNavigator = () => {
         headerStyle: { backgroundColor: "#351401" },
         headerTintColor: "white",
         sceneStyle: { backgroundColor: "#3f2f25" },
+        drawerContentStyle: { backgroundColor: "#451401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#451401",
+        drawerActiveBackgroundColor: "#e4baa1",
       }}
     >
       <Drawer.Screen
@@ -19,11 +24,20 @@ const DrawerNavigator = () => {
         name={DrawerScreenName.MealCategories}
         options={{
           title: "All Categories",
+          drawerIcon: ({ color, size }) => {
+            return <Ionicons name="list" color={color} size={size} />;
+          },
         }}
       />
       <Drawer.Screen
         component={FavoriteScreen}
         name={DrawerScreenName.Favorite}
+        options={{
+          title: "Favorites",
+          drawerIcon: ({ color, size }) => {
+            return <Ionicons name="star" color={color} size={size} />;
+          },
+        }}
       />
     </Drawer.Navigator>
   );
